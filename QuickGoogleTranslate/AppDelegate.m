b3531e2040e170b1efb04be0c4c9485ffdb44cd9
ultragtx <ultragtx@gmail.com>
2013-03-13 11:38:36 +0800

    Make active when click

--- QuickGoogleTranslate/AppDelegate.m
+++ QuickGoogleTranslate/AppDelegate.m
@@ -26,11 +26,15 @@
     self.window.attachedToMenuBar = YES;
     
     [_webView setCustomUserAgent:@"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7"];
+    
+//    [_webView setCustomUserAgent:@"Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"];
+
     [_webView setMainFrameURL:@"http://translate.google.com"];
+    
     
 }
 
-#pragma mark - 
+#pragma mark - Reload
 
 - (IBAction)reload:(id)sender {
     [_webView setMainFrameURL:@"http://translate.google.com"];
