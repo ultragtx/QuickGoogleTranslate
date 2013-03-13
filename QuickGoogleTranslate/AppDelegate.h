9d6775c941f89c9c498cf5fc4f9bda2e2229dfa2
ultragtx <ultragtx@gmail.com>
2013-03-13 10:05:07 +0800

    Initial Commit

---
+++ QuickGoogleTranslate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuickGoogleTranslate
+//
+//  Created by Xinrong Guo on 13-3-13.
+//  Copyright (c) 2013年 Xinrong Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ QuickGoogleTranslate/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  QuickGoogleTranslate
+//
+//  Created by Xinrong Guo on 13-3-13.
+//  Copyright (c) 2013年 Xinrong Guo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
